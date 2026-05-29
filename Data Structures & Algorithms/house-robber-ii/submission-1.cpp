afee1c198a98651ab20449fe92db05bab4ea5572
YU-KAI CHAN <100844645+Kevin20010912@users.noreply.github.com>
2026-05-29 19:01:16 +0800

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        vector<int> dp1(n + 1, 0), dp2(n + 1, 0);
+        dp1[0] = 0;
+        dp1[1] = nums[0];
+        for(int i = 2; i <= n - 1; i++){
+            dp1[i] = max(dp1[i - 2] + nums[i - 1], dp1[i - 1]);
+        }
+        dp2[0] = 0;
+        dp2[1] = 0;
+        for(int i = 2; i <= n; i++){
+            dp2[i] = max(dp2[i - 2] + nums[i - 1], dp2[i - 1]);
+        }
+        return max(dp1[n - 1], dp2[n]);
+        
+    }
+};
