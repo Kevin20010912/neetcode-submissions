06ac0179b345f276a13dfc62492eb918cc419fc0
YU-KAI CHAN <100844645+Kevin20010912@users.noreply.github.com>
2026-06-05 14:35:16 +0800

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int farthest = 0;
+        int count = 0;
+        int curEnd = 0;
+        if(nums.size() == 1){
+            return 0;
+        }
+        for(int i = 0; i < nums.size() - 1; i++){
+            farthest = max(farthest, i + nums[i]);
+            if(i == curEnd){
+                curEnd = farthest;
+                count++;
+            }
+            
+        }
+        return count;
+    }
+};
