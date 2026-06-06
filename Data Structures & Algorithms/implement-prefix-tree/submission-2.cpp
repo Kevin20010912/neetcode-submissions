69ea32ca92ba1fe34e075338339915bf7f93ac67
YU-KAI CHAN <100844645+Kevin20010912@users.noreply.github.com>
2026-06-06 13:35:10 +0800

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,51 @@
+class PrefixTree {
+    struct TrieNode{
+        TrieNode* children[26] = {nullptr};
+        bool isEnd = false;
+    };
+    TrieNode* head = new TrieNode();
+public:
+    PrefixTree() {
+        
+    }
+    
+    void insert(string word) {
+        TrieNode* cur = head;
+        for(char c : word){
+            int idx = c - 'a';
+            if(cur->children[idx] == nullptr){
+                cur->children[idx] = new TrieNode();
+            }
+            cur = cur->children[idx];
+        }
+        cur->isEnd = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* cur = head;
+        for(char c : word){
+            int idx = c - 'a';
+            if(cur->children[idx] == nullptr){
+                return false;
+            }
+            cur = cur->children[idx];
+        }
+        if(cur->isEnd){
+            return true;
+        }
+        return false;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* cur = head;
+        for(char c : prefix){
+            int idx = c - 'a';
+            if(cur->children[idx] == nullptr){
+                return false;
+            }
+            cur = cur->children[idx];
+        }
+        
+        return true;
+    }
+};
