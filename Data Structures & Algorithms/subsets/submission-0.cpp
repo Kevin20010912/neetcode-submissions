ca00e50c479c2938cf66a02e5490dd709a89ca77
YU-KAI CHAN <100844645+Kevin20010912@users.noreply.github.com>
2026-06-22 13:28:29 +0800

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    vector<vector<int>> res;
+    vector<int> path;
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+    void backtrack(vector<int>& nums, int start){
+        res.push_back(path);
+        for(int i = start; i < nums.size(); i++){
+            path.push_back(nums[i]);
+            backtrack(nums, i + 1);
+            path.pop_back();
+        }
+
+    }
+};
